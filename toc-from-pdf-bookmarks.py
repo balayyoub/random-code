56f8411c4d29f9187953b01614e3bbfcf679b568
balayyoub <balayyoub@gmail.com>
2021-09-30 09:29:52 +0300

    Update toc-from-pdf-bookmarks.py

--- toc-from-pdf-bookmarks.py
+++ toc-from-pdf-bookmarks.py
@@ -1,11 +1,13 @@
 import fitz
 
-file_name = "01-file_name.pdf"
+file_name = "01-file_name" + ".pdf"
 doc = fitz.open(file_name)
-f = open(file_name + "_toc.txt", "x")
+f = open(file_name + "_toc.txt", "x", encoding="utf-8")
 toc = doc.getToC()
 for t in toc:
     while t[0] > 1: 
         t[1] = '    ' + t[1]
         t[0] -= 1
-    print(t[1].ljust(70,'.'), t[2], file=f)
+    #t[1] = t[1] + ' '
+    #t[2] = ' ' + str(t[2])
+    print(t[1].ljust(80,'.'), t[2], file=f)
