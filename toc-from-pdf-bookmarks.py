2a7637db7e45c345c6f1af5c44f67d897dc5f0e7
balayyoub <balayyoub@gmail.com>
2021-09-30 10:04:23 +0300

    Update toc-from-pdf-bookmarks.py

--- toc-from-pdf-bookmarks.py
+++ toc-from-pdf-bookmarks.py
@@ -1,8 +1,9 @@
 import fitz
 
+max_width = 80
 file_name = "01-file_name" + ".pdf"
 doc = fitz.open(file_name)
-f = open(file_name + "_toc.txt", "x", encoding="utf-8")
+f = open(file_name + "_toc.txt", "w", encoding="utf-8")
 toc = doc.getToC()
 for t in toc:
     while t[0] > 1: 
@@ -10,4 +11,5 @@ for t in toc:
         t[0] -= 1
     #t[1] = t[1] + ' '
     #t[2] = ' ' + str(t[2])
-    print(t[1].ljust(80,'.'), t[2], file=f)
+    info = (t[1][:(max_width-1)]) if len(t[1]) > (max_width-1) else t[1]
+    print(info.ljust(max_width,'.'), t[2], file=f)
